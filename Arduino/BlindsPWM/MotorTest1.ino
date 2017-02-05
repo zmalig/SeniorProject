f5517dcf3ebaed0f1524ef6b08867a13c0968a81
NirajMorar <niraj.morar@gmail.com>
2017-02-04 16:40:45 -0800

    Add files via upload

---
+++ Arduino/BlindsPWM/MotorTest1.ino
@@ -0,0 +1,16 @@
+
+int motorDrive = 9;
+int timeDelay = 5000;
+
+void setup(){
+  pinMode(motorDrive, OUTPUT);
+}
+
+void loop(){
+  analogWrite(motorDrive, 255);
+  delay(timeDelay);
+  analogWrite(motorDrive, 128);
+  delay(timeDelay);
+  analogWrite(motorDrive, 0);
+  delay(timeDelay);
+}
