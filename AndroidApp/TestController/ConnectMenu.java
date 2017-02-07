4ce6086973137851ecb5cfca0d2f23f7ae092acf
zmalig <zacharymalig@gmail.com>
2017-02-07 15:31:05 -0800

    Update ConnectMenu.java

--- AndroidApp/TestController/ConnectMenu.java
+++ AndroidApp/TestController/ConnectMenu.java
@@ -1,4 +1,4 @@
-package com.zackmalig.blindscontroller;
+package com.librarian.zachary.blindscontroller;
 
 import android.content.Context;
 import android.content.Intent;
@@ -10,14 +10,18 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import java.io.DataOutputStream;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetAddress;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class ConnectMenu extends AppCompatActivity implements View.OnClickListener {
 
-    public final static String PREF_IP = "PREF_IP_ADDRESS";
-    public final static String PREF_PORT = "PREF_PORT_NUMBER";
+    public final static String PREF_IP = "192.168.1.103";
+    public final static int PREF_PORT = 4989;
 
     TextView messageText, testResult;
     Button sendData;
@@ -27,6 +31,8 @@ public class ConnectMenu extends AppCompatActivity implements View.OnClickListen
     // shared preferences to be saved when user closes the application
     SharedPreferences.Editor editor;
     SharedPreferences sharedPreferences;
+
+    private Socket socket;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,15 +62,18 @@ public class ConnectMenu extends AppCompatActivity implements View.OnClickListen
         portNumber = (EditText) findViewById(R.id.editTextPortNumber);
         parameterValue = (EditText) findViewById(R.id.editTextData);
 
-        // get IP address and port number from last user run
+        /* get IP address and port number from last user run
         ipAddress.setText(sharedPreferences.getString(PREF_IP, "IP Address"));
         portNumber.setText(sharedPreferences.getString(PREF_PORT,"Port #"));
+        */
+
+        new Thread(new ClientThread()).start();
     }
 
 
     @Override
     public void onClick(View view) {
-        // get ip address
+        /* get ip address
         String ip = ipAddress.getText().toString().trim();
         // get port number
         String portString = portNumber.getText().toString().trim();
@@ -74,18 +83,20 @@ public class ConnectMenu extends AppCompatActivity implements View.OnClickListen
         editor.putString(PREF_IP, ip);
         editor.putString(PREF_PORT, portString);
         editor.commit(); // save new values
+        */
 
         switch(view.getId()) {
             case R.id.button_sendData:
                 // get data value
-                String dataValue = parameterValue.getText().toString().trim();
+                // String dataValue = parameterValue.getText().toString().trim();
 
-                // output to socket
+                String dataValue = "{fuck you}";
                 try {
-                    int portValue = Integer.parseInt(portString);
-                    Socket socket = new Socket(ip, portValue);
-                    DataOutputStream DOS = new DataOutputStream(socket.getOutputStream());
-                    DOS.writeUTF(dataValue);
+                    // InetAddress serverAddress = InetAddress.getByName(ip);
+                    // int portValue = Integer.parseInt(portString);
+                    PrintWriter out = new PrintWriter(new BufferedWriter(
+                            new OutputStreamWriter(socket.getOutputStream())), true);
+                    out.println(dataValue);
 
                     // declare success
                     testResult.setText("Success!");
@@ -101,4 +112,18 @@ public class ConnectMenu extends AppCompatActivity implements View.OnClickListen
 
     }
 
+    class ClientThread implements Runnable {
+        @Override
+        public void run() {
+            try {
+                InetAddress serverAddr = InetAddress.getByName(PREF_IP);
+                socket = new Socket(serverAddr, PREF_PORT);
+            } catch (UnknownHostException e1) {
+                e1.printStackTrace();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
 }
+
